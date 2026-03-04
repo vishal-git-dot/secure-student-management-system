7520591e990d6de81e39eb853699aad0ab75e095
Vishal Purushothaman <vega1four@gmail.com>
2026-03-04 21:42:38 +0530

    Create index.php

---
+++ student-system/index.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php'; ?>
+
+<h2>Welcome</h2>
+<p>Simple Student Management System</p>
+
+<a href="login.php"><button>Login</button></a>
+
+<?php include 'includes/footer.php'; ?>
