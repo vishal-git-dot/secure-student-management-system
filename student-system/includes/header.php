72694524fa76d7e53e12020ffd2e9c7f271d6e76
Vishal Purushothaman <vega1four@gmail.com>
2026-03-04 21:43:14 +0530

    Create header.php

---
+++ student-system/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student System</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div class="navbar">
+    Student Management System
+</div>
+<div class="container">
